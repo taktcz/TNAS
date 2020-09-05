7d48b0ae1595b0ddb484d08f59d5af89d061e1b4
Tadeas Minha <trimen@pborky.sk>
2020-09-05 19:29:14 +0200

    Fixed typo in readme

--- README.ino
+++ README.ino
@@ -8,7 +8,7 @@
  *  Principle is simple. At first start array of unique random numbers is generated, then it is saved.
  *  With each door opening random value at random byte in first block is changend in patternt that was generated.
  *  This gave us aprox 4000 possibilities. If we limit time beetween reads on 20 seconds, attacker would neet ~22 hours to determine whole pattern which we use.
- *  Value and number of byte is saved for each card. If card is compromised than copied card stops working, if genuine card is read before copied one and vice versa.
+ *  Value and number of byte is saved for each card. If card is compromised then copied card stops working, if genuine card is read before copied one and vice versa.
  *  In either case we can easily determine which card was compromised, because it stop working for valid user or attacker for 4000 iterations.
  *  
  *  shuffle.c is C app that you can use for generation of array of unique random numbers from 1 to MAX.
