df887073accabacdb0f09a4bc25669832f28c4bf
trimenz <tadeas.minha@gmail.com>
2021-01-13 07:35:28 +0100

    Fixed typos in readme, added generate_random_string_bash.c

--- README.ino
+++ README.ino
@@ -6,7 +6,7 @@
  *  Keys have to be set in array keyA and keyB
  *  
  *  Principle is simple. At first start array of unique random numbers is generated, then it is saved.
- *  With each door opening random value at random byte in first block is changend in patternt that was generated.
+ *  With each door opening random value at random byte in first block is changed in pattern that was generated.
  *  This gave us aprox 4000 possibilities. If we limit time beetween reads on 20 seconds, attacker would neet ~22 hours to determine whole pattern which we use.
  *  Value and number of byte is saved for each card. If card is compromised then copied card stops working, if genuine card is read before copied one and vice versa.
  *  In either case we can easily determine which card was compromised, because it stop working for valid user or attacker for 4000 iterations.
